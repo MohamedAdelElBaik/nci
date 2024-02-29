0501d4e31ac500f5cedaeac4a14da770ee940aee
MohamedAdel310 <99290531+MohamedAdelElBaik@users.noreply.github.com>
2024-02-29 15:44:30 +0200

    STYLE : create grid box of dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,27 @@
+import ItemsStatus from './ItemsStatus';
+import style from './page.module.css';
+
 export default function Dashboard() {
-  return <div>page</div>;
+  return (
+    <section className={style.container}>
+      <div className={style.box1}>
+        <ItemsStatus />
+      </div>
+      <div className={style.box2}>
+        <h1>box2</h1>
+      </div>
+      <div className={style.box3}>
+        <h1>box3</h1>
+      </div>
+      <div className={style.box4}>
+        <h1>box4</h1>
+      </div>
+      <div className={style.box5}>
+        <h1>box5</h1>
+      </div>
+      <div className={style.box6}>
+        <h1>box6</h1>
+      </div>
+    </section>
+  );
 }
